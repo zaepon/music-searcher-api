b278b37c85bb3acc39e59b284ed77fa1033be10a
saakuru <miokillian@gmail.com>
2020-11-21 05:06:37 +0200

    feat: add cors implementation, and routes for refresh_token, Add Context

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,37 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { TestResolver, AuthResolver } from "./resolvers";
 import { initConnection } from "./database/index";
-import { getSpotifyUserProfile } from "./spotify/index"
-import { Context } from "./context";
+import cors from "cors";
+import { spotifyRequest } from "./spotify";
 require("dotenv").config();
 
 const main = async () => {
   await initConnection();
 
   const app = express();
+  app.use(
+    cors({
+      origin: process.env.WEB_ORIGIN,
+      credentials: true,
+    })
+  );
+
+  app.get("/", (_req, res) => {
+    res.send("API ONLINE");
+  });
+
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.headers.cookie?.split("gid=")[1];
+    if (!token || token !== "undefined") {
+      const request = await spotifyRequest("POST", "refresh_token", {
+        refresh_token: token,
+      });
+      return res.send({ accessToken: request.data.access_token });
+    } else {
+      return res.send({ accessToken: "" });
+    }
+  });
+
   app.listen(4000, () => {
     console.log("Started server on port 4000");
   });
@@ -21,13 +44,10 @@ const main = async () => {
       resolvers: [TestResolver, AuthResolver],
       validate: false,
     }),
-    context: async ({req}) => {
-      const ctx: Context = {user: await getSpotifyUserProfile(req.headers.authorization as string)}
-      return ctx
-    }
+    context: async ({ req, res }) => ({ req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.get("/", (_, res) => {
     res.send("Server is online");
