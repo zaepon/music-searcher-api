1b8830287d0f101d1aa5f238fadfe459da96eafa
saakuru <miokillian@gmail.com>
2020-11-21 05:05:42 +0200

    feat(resolvers): add cookie support to auth resolver getAccessToken, remove refreshToken mutation

--- src/resolvers/auth.ts
+++ src/resolvers/auth.ts
@@ -1,58 +1,25 @@
 import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
-import axios from "axios";
-import qs from "qs";
-import { DateTime } from "luxon";
-import { AuthResponse, RefreshTokenResponse } from "./types/auth";
-import {Context} from "../context";
+import { AuthResponse } from "./types/auth";
+import { ApolloError } from "apollo-server-express";
+import { Context } from "src/context";
+import { spotifyRequest } from "../spotify";
 
 @Resolver()
 export class AuthResolver {
-  async spotifyRequest(type: "GET" | "POST", grant_type: string, params: any) {
-    const options = {
-      method: type,
-      url: "https://accounts.spotify.com/api/token",
-      headers: {
-        Authorization: `${process.env.SPOTIFY_BASIC_AUTH}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      data: qs.stringify({
-        grant_type: grant_type,
-        redirect_uri: `${process.env.LOGIN_REDIRECT_URI}`,
-        ...params,
-      }),
-    };
-
-    return await axios(options);
-  }
 
   @Mutation(() => AuthResponse)
-  async getAccessToken(@Arg("code") code: string) {
-    const req = await this.spotifyRequest("POST", "authorization_code", {
+  async getAccessToken(@Ctx() ctx: Context, @Arg("code") code: string) {
+    
+    const req = await spotifyRequest("POST", "authorization_code", {
       code,
     });
-
+    if(!req.data) throw new ApolloError("something went wrong", "AUTH_FAIL")
+    //refreshToken: req.data.refresh_token,
     const response: AuthResponse = {
       token: `${req.data.token_type} ${req.data.access_token}`,
-      expires: DateTime.utc()
-        .plus({ seconds: req.data.expires_in })
-        .toSeconds(),
-      refreshToken: req.data.refresh_token,
+      expires: req.data.expires_in,      
     };
-
-    return response;
-  }
-
-  @Mutation(() => RefreshTokenResponse)
-  async refreshAccessToken(@Arg("refreshToken") refreshToken: string, @Ctx() ctx: Context) {
-    console.log(ctx)
-    const req = await this.spotifyRequest("POST", "refresh_token", {refresh_token: refreshToken})
-
-    const response: RefreshTokenResponse = {
-      token: `${req.data.token_type} ${req.data.access_token}`,
-      expires: DateTime.utc()
-        .plus({ seconds: req.data.expires_in }).toSeconds()
-    }
-
+    ctx.res.cookie("gid", req.data.refresh_token, {httpOnly: true, path: "/"})
     return response;
   }
 }
