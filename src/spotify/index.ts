c6e295741ba4e1ec67078714ae405e4705503225
saakuru <miokillian@gmail.com>
2020-11-21 05:05:05 +0200

    refactor: move spotifyRequest to spotify folder

--- src/spotify/index.ts
+++ src/spotify/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import qs from "qs";
 import {SpotifyUser} from "./types"
 
 export const getSpotifyUserProfile = async (token: string): Promise<SpotifyUser> => {
@@ -8,4 +9,25 @@ export const getSpotifyUserProfile = async (token: string): Promise<SpotifyUser>
     headers: {'Authorization': token}
   })
   return req.data;
+}
+
+export const  spotifyRequest = async (type: "GET" | "POST", grant_type: string, params: any) =>  {
+  const options = {
+    method: type,
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization: `${process.env.SPOTIFY_BASIC_AUTH}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: qs.stringify({
+      grant_type: grant_type,
+      redirect_uri: `${process.env.LOGIN_REDIRECT_URI}`,
+      ...params,
+    })
+  };
+  try{
+  return await axios(options);
+  }catch(e) {
+    return e;
+  }
 }
