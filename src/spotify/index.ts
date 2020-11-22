9a8116f32fd94db76cf513921838f413b21870a6
saakuru <miokillian@gmail.com>
2020-11-22 06:27:25 +0200

    feat: add SearchArtistByName & parseArtist implementation to Spotify contexts

--- src/spotify/index.ts
+++ src/spotify/index.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 import qs from "qs";
+import { Artist, ArtistsResponse } from "src/resolvers/types/artist";
+import { getData } from "../utils";
 import { SpotifyUser } from "./types";
+import {
+  SpotifyArtist,
+  SpotifyArtistImage,
+  SpotifyArtistResponse,
+} from "./types/artist";
 
 export const getSpotifyUserProfile = async (
   token: string
@@ -13,7 +20,7 @@ export const getSpotifyUserProfile = async (
   return req.data;
 };
 
-export const spotifyRequest = async (
+export const spotifyTokenRequest = async (
   type: "GET" | "POST",
   grant_type: string,
   params: any
@@ -36,4 +43,48 @@ export const spotifyRequest = async (
   } catch (e) {
     return e;
   }
+};
+
+export const searchArtistByName = async (
+  name: string,
+  token: string,
+  limit = 20,
+  offset = 0
+) => {
+  const searchRes = await getData(
+    `https://api.spotify.com/v1/search?q=${name}&type=artist&offset=${offset}&limit=${limit}`,
+    token,
+    900
+  );
+  const artistsData: { artists: SpotifyArtistResponse } = searchRes;
+
+  const parsedArtists = artistsData.artists.items.map((a) => parseArtist(a));
+
+  const res: ArtistsResponse = {
+    pages: {
+      start: artistsData.artists.offset,
+      limit: artistsData.artists.limit,
+      total: artistsData.artists.total,
+    },
+    artists: parsedArtists,
+  };
+
+  return res;
+};
+
+export const parseArtist = (artist: SpotifyArtist): Artist => {
+  let imageObj = artist.images!.filter(
+    (img: SpotifyArtistImage) => img.width === 640
+  );
+
+  const parsedArtist = {
+    genres: artist.genres,
+    href: artist.href,
+    id: artist.id,
+    image: imageObj[0]?.url,
+    name: artist.name,
+    spotifyUri: artist.uri,
+  };
+
+  return parsedArtist;
 };
