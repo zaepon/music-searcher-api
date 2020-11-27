e8c54b238fe8ab12265bddc13a13bdbb4f7a6fda
saakuru <miokillian@gmail.com>
2020-11-28 00:36:25 +0200

    feat(types): spotify album typedef

--- src/spotify/types/artist.ts
+++ src/spotify/types/artist.ts
@@ -1,6 +1,6 @@
-import { SpotifyGetResponse } from "./response"
+import { SpotifyGetResponse } from "./response";
 
-export interface SpotifyArtistImage {
+export interface SpotifyImage {
   height: number;
   url: string;
   width: number;
@@ -17,18 +17,51 @@ export interface SpotifyArtist {
   genres: string[];
   href: string;
   id: string;
-  images: SpotifyArtistImage[];
+  images: SpotifyImage[];
   name: string;
   popularity: number;
   type: string;
-  uri: string
+  uri: string;
+}
+
+export interface SpotifyAlbum {
+  album_group: string;
+  album_type: string;
+  artists: [
+    {
+      external_urls: {
+        spotify: string;
+      };
+      href: string;
+      id: string;
+      name: string;
+      type: string;
+      uri: string;
+    }
+  ];
+  available_markets: string[];
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: SpotifyImage[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  total_tracks: number;
+  type: string;
+  uri: string;
 }
 
 export interface SpotifyArtistResponse extends SpotifyGetResponse {
-  items: SpotifyArtist[]
+  items: SpotifyArtist[];
 }
 
 export interface SpotifySimilarArtistResponse {
-  artists: SpotifyArtist[]
+  artists: SpotifyArtist[];
 }
 
+export interface SpotifyArtistAlbumResponse extends SpotifyGetResponse {
+  items: SpotifyAlbum[];
+}
